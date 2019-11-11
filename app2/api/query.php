537e3d762d00072d4966f1d115499793bcefcd11
iamdoris1993 <dolly.chen702@gmail.com>
2019-11-11 16:29:26 +0800

    撰寫班級資料查詢API

--- app2/api/query.php
+++ app2/api/query.php
@@ -1,5 +1,30 @@
 <?php
 //處理查詢資料的請求
+$dsn="mysql:host=localhost;charset=utf8;dbname=ajax";
+$pdo=new PDO($dsn,"root","iamdoris19930303");
 
+$class='1'.sprintf("%02d",$_GET["class"]);
+
+//撈出班級學生
+$sql="select * from students where substring(`class_num`,1,3)='$class'";
+$rows=$pdo->query($sql)->fetchAll();
+
+echo "<tr>";
+echo "<td>座號</td>";
+echo "<td>姓名</td>";
+echo "<td>科系</td>";
+echo "<td>生日</td>";
+echo "<td>學號</td>";
+echo "</tr>";
+
+foreach($rows as $r){
+    echo "<tr>";
+    echo "<td>".mb_substr($r['class_num'],3,2,"utf8")."</td>";
+    echo "<td>".$r['name']."</td>";
+    echo "<td>".$r['dept']."</td>";
+    echo "<td>".$r['birthday']."</td>";
+    echo "<td>".$r['uni_id']."</td>";
+    echo "</tr>";
+}
 
 ?>
